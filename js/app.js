81ac65ac0a19c6464f9c86b566e3e2db19bf7edd
Michele Cheow <ldnallen@gmail.com>
2017-10-18 17:25:44 -0700

    Added more products and repeat directive to handle the view

--- js/app.js
+++ js/app.js
@@ -2,13 +2,26 @@
 var app = angular.module('store', []);
 
 app.controller('StoreController', function(){
-	this.product = gem;
+	this.products = gems;
 });
 
-var gem = {
+var gems = [
+
+{
 	name: 'Dodecahedron',
 	price: 2.95,
 	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lobortis lacus quis tortor gravida malesuada et quis nunc. Sed eu commodo risus. Vestibulum non lacus fringilla, mattis risus at, pharetra urna.',
+	canPurchase: false,
+	soldOut: true
+},
+{
+	name: 'Pentagonal Gem',
+	price: 5.95,
+	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lobortis lacus quis tortor gravida malesuada et quis nunc. Sed eu commodo risus. Vestibulum non lacus fringilla, mattis risus at, pharetra urna.',
+	canPurchase: false,
+	soldOut: true
 }
+
+]
 
 })();
