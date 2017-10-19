a471884dde61be405eaa94e5066462009845a8fc
Michele Cheow <ldnallen@gmail.com>
2017-10-18 17:11:28 -0700

    Added Controller functionality and added to DOM

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
+(function(){
 var app = angular.module('store', []);
+
+app.controller('StoreController', function(){
+	this.product = gem;
+});
+
+var gem = {
+	name: 'Dodecahedron',
+	price: 2.95,
+	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lobortis lacus quis tortor gravida malesuada et quis nunc. Sed eu commodo risus. Vestibulum non lacus fringilla, mattis risus at, pharetra urna.',
+}
+
+})();
